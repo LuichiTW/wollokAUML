eee879ae153a2599b81d8befb68dee56c2a16174
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-09-21 02:04:31 -0300

    funciones para buscar metodos repetidos

--- src/interfaces.c
+++ src/interfaces.c
@@ -4,14 +4,55 @@
 
 extern objetos *lista;
 
+typedef struct interfaces{
+    char objetos[150];
+    char metodos[150];
+    struct  interfaces *siguiente;
+}interfaces;
+
+interfaces *listaInterfaces;
+
 void crearInterfaces(){
+    //inicializar la lista de interfaces
+    limpiarLinea(listaInterfaces->objetos);
+    limpiarLinea(listaInterfaces->metodos);
+    listaInterfaces->siguiente = NULL;
+
     objetos *aux = lista;
 
     while (aux->siguiente != NULL) {
         //verifica que tenga metodos
         if (aux->metodos[0] != '\0') {
             char *metodo = strtok(aux->metodos, "\n");
+            while(metodo != NULL){
+                buscarCoincidencias(aux->nombre ,metodo);
+                metodo = strtok(NULL, "\n");
+            }
         }
         aux = aux->siguiente; 
     }
+}
+
+void buscarCoincidencias(char nombre[50], char *metodo){
+    objetos *auxiliar = lista;
+    while(auxiliar->siguiente != NULL){
+        if (strcmp(auxiliar->nombre, nombre) == 0) {
+            if (auxiliar->siguiente != NULL) {
+                auxiliar = auxiliar->siguiente;
+            }else {
+            break;
+            }
+        }
+
+        if (tieneMetodo(auxiliar->nombre, metodo)) {
+        
+        }
+        
+        auxiliar = auxiliar->siguiente;
+    }
+    
+}
+
+int tieneMetodo(char nombre[50], char *metodo){
+    return 0;
 }
--- src/interfaces.h
+++ src/interfaces.h
@@ -3,6 +3,9 @@
 
 #include <string.h>
 
+
 void crearInterfaces();
+void buscarCoincidencias(char nombre[50], char *metodo);
+int tieneMetodo(char nombre[50], char *metodo);
 
 #endif //INTERFACES_H
